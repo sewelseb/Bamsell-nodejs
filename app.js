547407ade9abf33beaaec8f29189af4c4b1f76d7
Sébastien de Beauffort <sewelseb@hotmail.com>
2017-09-15 12:08:04 +0200

    youhouu, api json

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var clicks = require('./routes/api/click');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api/click/', clicks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api/click.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET create click entry. */
+router.get('/add', function(req, res, next) {
+    res.json({
+        message: 'click created'
+    });
+
+});
+
+module.exports = router;
