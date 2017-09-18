a073ac897e33698f749425556652a46ea995820b
setheal <setheal@users.noreply.github.com>
2017-09-18 17:22:34 +0200

    commit

--- routes/api/click.js
+++ routes/api/click.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var ClickModel = require('../model/click');
+var model = require('../../model/clicksModel');
 
 /* GET create click entry. */
 router.get('/add', function(req, res, next) {
     console.log(req);
-    var regClick = new ClickModel({userId:1, clickEvent:null})
+    var regClick = new model.ClickModel({userId:1, clickEvent:null})
     regClick.save(function(err, regClick){
       console.log('save');
       if(err)
