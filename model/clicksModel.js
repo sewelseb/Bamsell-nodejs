dd7601bdf13dbe3ea058dc85dc479743636c127e
setheal <setheal@users.noreply.github.com>
2017-09-18 17:37:01 +0200

    timestamp

--- model/clicksModel.js
+++ model/clicksModel.js
@@ -2,8 +2,13 @@ var mongoose = require('mongoose');
 var clickSchema = new mongoose.Schema({
   //Define the attributes of a click
   userId: Number,
+
   clickEvent: {
     //Define the attributes of events
+  }
+},{
+  timestamps:{
+    createdAt: 'created_at'
   }
 });
 var ClickModel = mongoose.model('Click', clickSchema);
