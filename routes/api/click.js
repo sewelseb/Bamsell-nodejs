4fca70c2cd6073bc19a43e86ba8b3f678abeb938
Sébastien de Beauffort <sewelseb@hotmail.com>
2017-09-18 17:49:53 +0200

    commit

--- routes/api/click.js
+++ routes/api/click.js
@@ -7,7 +7,7 @@ var model = require('../../model/clicksModel');
 
 /* GET create click entry. */
 router.get('/add', function(req, res, next) {
-    console.log(req);
+    //console.log(req);
     var regClick = new model.ClickModel({userId:1, clickEvent:null})
     regClick.save(function(err, regClick){
       console.log('save');
